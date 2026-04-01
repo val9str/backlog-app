7cf7abb14da44ede5506888162c04e7c5da1c81b
val9str <danil.peter2012@yandex.com>
2026-04-01 15:20:07 +0300

    Update log.js

--- public/js/log.js
+++ public/js/log.js
@@ -1,1 +1,22 @@
+let logData = [];
+
+function renderLog() {
+  const list = document.getElementById("log-list");
+
+  list.innerHTML = logData
+    .map(l => `
+      <div class="card">
+        <div class="card-title">${l.action}</div>
+        <div class="card-section"><strong>Дата:</strong> ${new Date(l.time).toLocaleString()}</div>
+      </div>
+    `)
+    .join("");
+}
+
+async function initLog() {
+  logData = await apiGet("log");
+  renderLog();
+}
+
+document.addEventListener("DOMContentLoaded", initLog);
 
