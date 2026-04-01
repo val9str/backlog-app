9246c3022f43d91d4f9b2e915a1e370d49d8b899
val9str <danil.peter2012@yandex.com>
2026-04-01 15:18:15 +0300

    Update api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,13 @@
+async function apiGet(name) {
+  const res = await fetch(`/api/${name}`);
+  return await res.json();
+}
+
+async function apiSave(name, data) {
+  await fetch(`/api/${name}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+}
 
