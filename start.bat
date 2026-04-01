3a7879753227c2cff77b35cc2106583daa1ff9d1
val9str <danil.peter2012@yandex.com>
2026-04-01 15:16:36 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,18 @@
+@echo off
+echo Checking Node.js...
+node -v >nul 2>&1
+if errorlevel 1 (
+    echo Node.js не найден. Установите Node.js с https://nodejs.org
+    pause
+    exit /b
+)
+
+if not exist node_modules (
+    echo Устанавливаю зависимости...
+    npm install
+)
+
+start http://localhost:3000
+node server.js
+pause
 
